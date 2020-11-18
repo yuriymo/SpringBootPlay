86a357b797d4eb0f71ca02fac378ea51005e923d
Yuriy Movchan <67926237+yuriymo@users.noreply.github.com>
2020-11-18 20:43:27 +0200

    add test data to db

--- src/main/java/org/mmy/springboot/Application.java
+++ src/main/java/org/mmy/springboot/Application.java
@@ -1,14 +1,55 @@
 package org.mmy.springboot;
 
+import com.google.common.collect.Streams;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@Slf4j
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initDB(CustomerRepository repository) {
+        return (args) -> {
+            // save a few customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+            repository.save(new Customer("Serg", "Plotkin"));
+            // fetch all customers
+            log.info("-------------------------------");
+            Streams.stream(repository.findAll())
+                    .map(Object::toString)
+                    .forEach(log::info);
+            log.info("-------------------------------");
+//            log.info("");
+//            // fetch an individual customer by ID
+//            Customer customer = repository.findById(1L);
+//            log.info("Customer found with findById(1L):");
+//            log.info("--------------------------------");
+//            log.info(customer.toString());
+//            log.info("");
+//
+//            // fetch customers by last name
+//            log.info("Customer found with findByLastName('Bauer'):");
+//            log.info("--------------------------------------------");
+//            repository.findByLastName("Bauer").forEach(bauer -> {
+//                log.info(bauer.toString());
+//            });
+//            // for (Customer bauer : repository.findByLastName("Bauer")) {
+//            //  log.info(bauer.toString());
+//            // }
+//            log.info("");
+        };
     }
 
 //    @Bean
@@ -20,5 +61,4 @@ public class Application {
 //                    .forEach(log::info);
 //        };
 //    }
-
 }
